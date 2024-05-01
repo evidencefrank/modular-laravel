6f8d37f64b6c09801a2ca796564196544dddfc4d
Evidence Mandizvidza <evidence@lulaloop.co.za>
2024-05-01 10:12:15 +0200

    set up service providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,5 +1,15 @@
 <?php
 
+use Modules\Order\Providers\OrderServiceProvider;
+use Modules\Product\Providers\ProductServiceProvider;
+use Modules\Shipment\Providers\ShipmentServiceProvider;
+
 return [
+    /**
+     * Application Service Providers...
+    */
     App\Providers\AppServiceProvider::class,
+    OrderServiceProvider::class,
+    ProductServiceProvider::class,
+    ShipmentServiceProvider::class
 ];
